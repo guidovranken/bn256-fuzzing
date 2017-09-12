50de54d5a12742e27d79a81126d4c75c5165eadc
Guido Vranken <guidovranken@gmail.com>
2017-09-12 16:23:17 +0200

    Update fuzzer.c to incorporate Go BN256

--- fuzzer.c
+++ fuzzer.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include "bn256_instrumented.h"
 
 extern void runbn256(const uint8_t* data, size_t len);
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     runbn256(data, size);
-    return 0;
+    GoSlice p1 = {(void*)data, size, size};
+    GoInt ret = Fuzz(p1);
+    return (int)ret;
 }
